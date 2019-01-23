14bf30b07ecc21192876010c09dd470b29020b52
dani <dani.zaghian@gmail.com>
2019-01-23 11:15:27 -0800

    adds seed dump

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,35 @@
+Contact.create!([
+  {first_name: "Charlie", last_name: "Pineapple", email: "charlie@gmail.com", phone_number: "123-4567", middle_name: "Clarence", bio: "Not much into health food, is into champagne."},
+  {first_name: "Sarah", last_name: "Hoopes", email: "sarah@gmail.com", phone_number: "555-5555", middle_name: "Ace", bio: "Loves pina coladas and getting caught in the rain."},
+  {first_name: "Lilly", last_name: "Winfree", email: "lilly@gmail.com", phone_number: "777-7777", middle_name: "Ace", bio: "Loves pina coladas and getting caught in the rain."},
+  {first_name: "Amanda", last_name: "Hale", email: "amanda@gmail.com", phone_number: "444-4444", middle_name: "Ace", bio: "Loves pina coladas and getting caught in the rain."},
+  {first_name: "Michael", last_name: "Dotson", email: "michael@gmail.com", phone_number: "333-3333", middle_name: "Ace", bio: "Loves pina coladas and getting caught in the rain."},
+  {first_name: "Amanda", last_name: "Hale", email: "amanda@gmail.com", phone_number: "444-4444", middle_name: "Ace", bio: "Loves pina coladas and getting caught in the rain."},
+  {first_name: "Michael", last_name: "Dotson", email: "michael@gmail.com", phone_number: "333-3333", middle_name: "Ace", bio: "Loves pina coladas and getting caught in the rain."},
+  {first_name: "Rachel", last_name: nil, email: "raquelita@gmail.com", phone_number: "888-8888", middle_name: "Ace", bio: "Loves pina coladas and getting caught in the rain."},
+  {first_name: "Pamela", last_name: "Hammes", email: "diedre@yahoo.com", phone_number: "218-343-8683", middle_name: "Ace", bio: "Loves pina coladas and getting caught in the rain."},
+  {first_name: "Taylor", last_name: "Watsica", email: "amada@gmail.com", phone_number: "578-289-6233", middle_name: "Ace", bio: "Loves pina coladas and getting caught in the rain."},
+  {first_name: "Isaias", last_name: "Hoppe", email: "phuong@hotmail.com", phone_number: "910-513-4699", middle_name: "Ace", bio: "Loves pina coladas and getting caught in the rain."},
+  {first_name: "Jimmy", last_name: "Kemmer", email: "arnulfo.terry@yahoo.com", phone_number: "687-348-4506", middle_name: "Ace", bio: "Loves pina coladas and getting caught in the rain."},
+  {first_name: "Berry", last_name: "Simonis", email: "muoi_braun@yahoo.com", phone_number: "821-252-6946", middle_name: "Ace", bio: "Loves pina coladas and getting caught in the rain."},
+  {first_name: "Antonina", last_name: "Streich", email: "ebonie.kunze@hotmail.com", phone_number: "559-875-2873", middle_name: "Ace", bio: "Loves pina coladas and getting caught in the rain."},
+  {first_name: "Melinda", last_name: "Rosenbaum", email: "mckinley@hotmail.com", phone_number: "731-799-6894", middle_name: "Ace", bio: "Loves pina coladas and getting caught in the rain."},
+  {first_name: "Spencer", last_name: "Conroy", email: "leon.bayer@yahoo.com", phone_number: "205-326-9319", middle_name: "Ace", bio: "Loves pina coladas and getting caught in the rain."},
+  {first_name: "Jann", last_name: "Kreiger", email: "jordan@yahoo.com", phone_number: "429-390-6962", middle_name: "Ace", bio: "Loves pina coladas and getting caught in the rain."},
+  {first_name: "Pearl", last_name: "Romaguera", email: "else_gerhold@yahoo.com", phone_number: "285-899-3981", middle_name: "Ace", bio: "Loves pina coladas and getting caught in the rain."},
+  {first_name: "Joesph", last_name: "Marvin", email: "lanny_hessel@gmail.com", phone_number: "225-324-6519", middle_name: "Ace", bio: "Loves pina coladas and getting caught in the rain."},
+  {first_name: "Ace", last_name: "Zaghian", email: "ace@gmail.com", phone_number: "999-9999", middle_name: "Boogie", bio: "Loves pina coladas and getting caught in the rain."},
+  {first_name: "Kelly", last_name: "Callaway", email: "kelly@gmail.com", phone_number: "222-2222", middle_name: "Hannah", bio: "Loves pina coladas and getting caught in the rain."},
+  {first_name: "Charlie", last_name: "Pineapple", email: "charlie@gmail.com", phone_number: "123-4567", middle_name: "Hannah", bio: "Loves pina coladas and getting caught in the rain."}
+])
 
-10.times do
-	contact = Contact.new(
-		first_name: FFaker::Name.first_name, 
-		last_name: FFaker::Name.last_name, 
-		email: FFaker::Internet.free_email, 
-		phone_number: FFaker::PhoneNumber.short_phone_number
-	)
-	contact.save
-end
+
+# 10.times do
+# 	contact = Contact.new(
+# 		first_name: FFaker::Name.first_name, 
+# 		last_name: FFaker::Name.last_name, 
+# 		email: FFaker::Internet.free_email, 
+# 		phone_number: FFaker::PhoneNumber.short_phone_number
+# 	)
+# 	contact.save
+# end
