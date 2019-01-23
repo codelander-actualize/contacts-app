486d3d36d7d426f14766ea0259466f32dc9ffd75
dani <dani.zaghian@gmail.com>
2019-01-23 10:53:52 -0800

    adds migrations

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -10,8 +10,14 @@ class Api::ContactsController < ApplicationController
     	first_name: params[:first_name],
     	last_name: params[:last_name],
     	email: params[:email],
-    	phone_number: params[:phone_number]
+    	phone_number: params[:phone_number],
+      bio: params[:bio]
     )
+
+    if params[:middle_name]
+      @contact.middle_name = params[:middle_name]
+    end
+
     @contact.save
     render 'show.json.jbuilder'
   end
@@ -25,9 +31,11 @@ class Api::ContactsController < ApplicationController
     @contact = Contact.find(params[:id])
 
     @contact.first_name = params[:first_name] || @contact.first_name
+    @contact.middle_name = params[:middle_name] || @contact.middle_name
     @contact.last_name = params[:last_name] || @contact.last_name
     @contact.email = params[:email] || @contact.email
     @contact.phone_number = params[:phone_number] || @contact.phone_number
+    @contact.bio = params[:bio] || @contact.bio
 
     @contact.save
     render 'show.json.jbuilder'
